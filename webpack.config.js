3c54186619686e8fbd2607bd950c5908559f99db
rnsijk <ramanarayana.sarma@weinvest.net>
2018-11-19 11:55:50 +0530

    Dev server setup with hot reloading

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/main.js','./scss/main.scss'],
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {sourceMap: true},
+          },
+          {
+            loader: 'sass-loader',
+            options: {sourceMap: true},
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['react', 'latest', 'stage-3']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+};
